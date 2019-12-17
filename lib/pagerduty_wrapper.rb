f267b5c0904173a7d13ebeda1b4c4d3f564cadf2
Sameer Mehta <sameer.mehta@techholding.co>
2019-12-17 16:49:40 +0530

    Initial Commit

---
+++ lib/pagerduty_wrapper.rb
@@ -0,0 +1,6 @@
+require "pagerduty_wrapper/version"
+
+module PagerdutyWrapper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pagerduty_wrapper/version.rb
@@ -0,0 +1,3 @@
+module PagerdutyWrapper
+  VERSION = "0.1.0"
+end
